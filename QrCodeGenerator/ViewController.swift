43d5783e0de4238e306c3acbd0484a3f0b99e0bf
claudioitalian12 <dark.claudio12@gmail.com>
2019-01-12 17:20:22 +0100

    0.0.1

--- QrCodeGenerator/ViewController.swift
+++ QrCodeGenerator/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qrCode: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        DispatchQueue.main.async {
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator", parameters: ["inputMessage" : "hello world".data(using: .utf8), "inputCorrectionLevel":"H"])
+            
+            let image = filter!.outputImage!
+            
+            let scalex = self.qrCode.frame.size.width / image.extent.size.width
+            let scaley = self.qrCode.frame.size.height / image.extent.size.height
+            let trasform = CGAffineTransform(scaleX: scalex, y: scaley)
+            
+            let qr = image.transformed(by: trasform)
+            
+            self.qrCode.image = UIImage(ciImage: qr)
+            
+        }
+    
     }
-
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        qrCode.frame.size.width = 240
+        qrCode.frame.size.height = 240
+        qrCode.center = view.center
+        
+    }
 
 }
 
